5a69cf4a9e91d24bd23d39a702685a4e37f533e6
Edward Atkinson <98823225+edatkinson@users.noreply.github.com>
2025-01-17 13:09:03 +0000

    Update process_cadd.py

--- canDrivr/CADD_Data/process_cadd.py
+++ canDrivr/CADD_Data/process_cadd.py
@@ -24,8 +24,32 @@ def merge_cadd(cadd_file, cadd_file2):
 
     return pd.DataFrame(cadd)
 
-combined_data = merge_cadd('cadd_simulated_data.tsv', 'cadd_human_data.tsv')
+# combined_data = merge_cadd('cadd_simulated_data.tsv', 'cadd_human_data.tsv')
 
-combined_data.to_csv('combined_cadd_data.tsv', sep='\t', index=False)
+# combined_data.to_csv('combined_cadd_data.tsv', sep='\t', index=False)
 
+def calc_length_indel(combined_file):
+    # Read the input file
+    data = pd.read_csv(combined_file, sep='\t')
+
+    # Calculate lengths for ref and alt strings
+    data['length_ref'] = data['ref'].str.len()
+    data['length_alt'] = data['alt'].str.len()
+
+    # Calculate the absolute difference in lengths
+    data['lengths'] = (data['length_ref'] - data['length_alt']).abs()
+
+    # Determine 'end' points based on insertion or deletion
+    data['end'] = data['pos'] + data['lengths'] - 1  # Default case for deletions
+    is_insertion = data['length_alt'] > data['length_ref']  # Identify insertions
+    data.loc[is_insertion, 'end'] = data.loc[is_insertion, 'pos'] + 1  # Adjust for insertions
+
+    # Drop intermediate columns and rename as needed
+    data.drop(columns=['length_ref', 'length_alt', 'lengths'], inplace=True)
+    data.rename(columns={'pos': 'start'}, inplace=True)
+
+    # Reorder columns to the desired format
+    data = data[['chrom', 'start', 'end', 'ref', 'alt', 'driver_stat']]
+
+    return data
 
